c2a9aef8d631a0f61a5785384840b8e3d22c8932
DorBergel <dor.bergel9@gmail.com>
2025-04-27 09:57:25 +0300

    feat: Update frontend dependencies and scripts; add JWT handling for contacts
    - Added new dependencies for JWT handling and browser compatibility in package.json.
    - Updated npm scripts to use react-app-rewired for better customization.
    - Implemented fetching of user contacts in MainScreen component using JWT for authorization.
    - Displayed contacts list in the MainScreen with appropriate handling for empty states.
    - Refactored API service functions to use ES6 export syntax.

--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -2,15 +2,16 @@ const User = require("../models/User");
 require("dotenv").config({ path: "../config/.env" });
 
 exports.getUserContacts = async (userId) => {
+  // Find the user by ID and populate the contacts field
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    // Find the user by ID and populate the contacts field
-    const user = await User.findById(userId);
-    if(!user) {
-        throw new Error("User not found");
-    }
+  contacts = await User.find({ _id: { $in: user.contacts } }).populate(
+    "contacts"
+  );
 
-    contacts = await User.find({ _id: { $in: user.contacts } });
-
-    // Return the contacts of the user
-    return user.contacts;
-}
+  // Return the contacts of the user
+  return contacts;
+};
