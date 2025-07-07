d0bf7bbb88b3abb356f385dbd6befa1e8915528b
DorBergel <dor.bergel9@gmail.com>
2025-07-07 11:42:09 +0300

    fix: update default server port from 5000 to 3025

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use("/api/message", require("./routes/message"));
 initializeChatWebSocket(server);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3025;
 server.listen(PORT, (error) => {
   if (!error) {
     logger.logInfoMsg(`Server is running on port ${PORT}`);
